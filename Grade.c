d6288af86ab8c7b1667ad43cd3aa1033c6fa3dc2
Prakriti-ac <68378939+Prakriti-ac@users.noreply.github.com>
2020-07-16 16:54:19 +0530

    Create grade.c
    Grades the students based on their performance.

---
+++ Grade.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+    int m;
+    printf("Enter your marks ");
+    scanf("%d",&);
+    if(m<0 || m>100)
+    printf("Wrong Entry");
+    else
+    {
+       else if(m<=40)
+       {
+        printf("Grade F");
+       }
+       else if(marks>=41 && marks<54)
+       {
+        printf("Grade D");
+       }
+       else if(marks>=55 && marks<69)
+       {
+        printf("Grade C");
+       }
+       else if(marks>=70 && marks<84)
+       {
+        printf("Grade B");
+       }
+       else if(marks>=85)
+       {
+        printf("Grade A");
+       }
+    }
+}
